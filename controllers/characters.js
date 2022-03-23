0be619e27153f082fd7b87cacb848300e439de6a
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-03-23 07:09:35 -0300

    crud-characters

--- controllers/characters.js
+++ controllers/characters.js
@@ -16,4 +16,40 @@ const getCharacters = async (req, res) => {
     res.status(500).json({ response });
   }
 };
-module.exports = { getCharacters };
+
+const createCharacters = async (req, res) => {
+  try {
+    const character = await Characters.create(req.body);
+    res.status(201).json(character);
+  } catch (error) {
+    res.status(404).jon("The character could not be created");
+  }
+};
+
+const updateCharacters = async (req, res) => {
+  const character = await Characters.findByPk(req.params.id);
+  if (!character) {
+    res.status(404).json("The character does not exist");
+  } else {
+    await Characters.update(req.body, {
+      where: { id: req.params.id },
+    });
+    res.status(201).json({ msg: "Character updated" });
+  }
+};
+const deleteCharacter = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Characters.destroy({ where: { id } });
+    res.status(201).json("Character was deleted");
+  } catch (error) {
+    res.status(404).json("Character does not exist");
+  }
+};
+
+module.exports = {
+  getCharacters,
+  createCharacters,
+  updateCharacters,
+  deleteCharacter,
+};
