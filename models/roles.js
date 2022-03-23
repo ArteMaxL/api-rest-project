751baadb0bf1a08b7e901fefce18d18064d39d07
ArteMaxL <artemiolucero@gmail.com>
2022-03-23 14:15:36 -0300

    Added relationships between Users and Roles

--- models/roles.js
+++ models/roles.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
-            Roles.belongsTo(models.Users, { as: 'roles' })
+            Roles.hasOne(models.Users);
         }
     }
     Roles.init({
--- models/users.js
+++ models/users.js
@@ -1,25 +1,26 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Users extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class Users extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            Users.belongsTo(models.Roles);
+        }
     }
-  }
-  Users.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    id_role: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Users',
-  });
-  return Users;
+    Users.init({
+        name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        id_role: DataTypes.INTEGER
+    }, {
+        sequelize,
+        modelName: 'Users',
+    });
+    return Users;
 };
